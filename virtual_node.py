7e824cb9cc7b0f925f609e068ec9f40d8f923c18
mv.pranay <mvpranavy@gmail.com>
2025-10-16 16:04:56 +0530

    implemented flooding, handling configuration change, need to implement receiving link state messages

---
+++ virtual_node.py
@@ -0,0 +1,98 @@
+import socket
+from select import select
+from time import time
+
+PERIODIC_FLOOD = 10
+
+IP_ADDR_ORACLE = input("Enter IP Address of Oracle : ")
+TCP_PORT_ORACLE = input("Enter TCP Port of Oracle : ")
+IP_ADDR = input("Enter my IP Address : ")
+UDP_PORT = input("Enter My UDP Port :")
+IP_ADDR_ORACLE = "172.25.153.68"
+
+# process configuration message from oracle node, return self_id and neighbour_info
+def process_link_state_info(link_state_info):
+    info = link_state_info.split(';')
+    self_id = int(info[0].split(',')[0])
+    neighbour_info = {}
+
+    for other_node_info in info[1:]:
+        neigh_id, neigh_ip, neigh_udp_port, cost = other_node_info.split(',')
+        neigh_id = int(neigh_id)
+        neigh_udp_port = int(neigh_udp_port)
+        cost = int(cost)
+        neighbour_info[neigh_id] = (neigh_ip, neigh_udp_port)
+
+        # store cost into global information structure
+        adj_list[self_id][neigh_id] = cost
+        adj_list[neigh_id][self_id] = cost
+
+    return self_id, neighbour_info
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.connect((IP_ADDR_ORACLE, TCP_PORT_ORACLE))
+tcp_socket.send(f"{IP_ADDR},{UDP_PORT}".encode())
+link_state_info = tcp_socket.recv(1024).decode()
+
+self_id, neighbour_info = process_link_state_info(link_state_info)
+
+# for global information
+# adj_list[i][j] -> cost from i to j
+adj_list = [[-1 for _ in range(26)] for _ in range(26)]
+
+# seq num of last msg seen from that node, for other nodes, and curr seq num for this node
+seq_num = {}
+seq_num[self_id] = 0
+
+process_link_state_info(link_state_info)
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind((IP_ADDR, UDP_PORT))
+
+# function to generate LSP
+def generateLSP():
+    packet = f"{self_id}|{seq_num[self_id]}|{adj_list[self_id]}"
+    seq_num[self_id] += 1
+    return packet
+
+# function to flood LSPs to neighbours
+def flood(dont_send=-1):
+    packet = generateLSP()
+    for neigh in neighbour_info:
+        if neigh == dont_send:
+            continue
+        udp_socket.sendto(packet.encode(), neighbour_info[neigh])
+    
+def handle_update_ls():
+    return 
+
+last_time = time()
+
+while True:
+    # 1. listen on tcp port for topo changes
+    # 2. listen on udp port for flooding messages
+    # 3. periodically flood the network with self link state
+
+    read_fds, _, _ = select([tcp_socket, udp_socket], [], [], PERIODIC_FLOOD)
+
+    curr_time = time()
+
+    # timeout occurred, send flooding messages to neighbours
+    if curr_time - last_time > PERIODIC_FLOOD:
+        flood()
+    else:
+        # update link state of current node, adj_list and flood LSPs
+        if tcp_socket in read_fds:
+            for i in range(26):
+                adj_list[self_id][i] = -1
+
+            link_state_info = tcp_socket.recv(1024).decode()
+            _, neighbour_info = process_link_state_info(link_state_info)
+            
+            flood()
+
+        # store info, forward if needed
+        if udp_socket in read_fds:
+            handle_update_ls()
+        
+    last_time = curr_time
